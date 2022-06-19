79d87608de9a3060d1050ce716b7e4fbc774586f
Mark Rivera <mrivera7475@gmail.com>
2022-06-19 00:17:16 -0400

    created switch statement

---
+++ SwitchStatement/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("What is your favorite school subject?");
+var response = Console.ReadLine();
+
+switch (response)
+{
+    case "Science":
+        Console.WriteLine("Cool! It's my favorite too");
+        break;
+
+    case "Math":
+        Console.WriteLine("Nice, this is my second favorite");
+        break;
+
+    case "Art":
+        Console.WriteLine("I like Art, but not as much as Science & Math");
+        break;
+
+    case "English":
+        Console.WriteLine("That's Awesome!");
+        break;
+
+    case "History":
+        Console.WriteLine("Thats cool!");
+        break;
+
+    default:
+        Console.WriteLine("Really I did not have to take that one");
+        break;
+
+}
